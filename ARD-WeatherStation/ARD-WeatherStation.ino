15da059f221a0543ec7fcf67ed348e4306d5ade9
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2017-02-14 17:57:05 +0700

    finish

--- ARD-WeatherStation/ARD-WeatherStation.ino
+++ ARD-WeatherStation/ARD-WeatherStation.ino
@@ -1,6 +1,8 @@
 /*
- * Weather Station by IVEN1
- * 
+   Weather Station by IVEN1
+
+   Button LCD black light : digital pin 7
+
    The LCD1602_I2C circuit :
    VCC  -  +5 V
    GND  -  Ground
@@ -29,12 +31,13 @@
 
 LiquidCrystal_I2C lcd(0x3f, 16, 2);
 
-#define CS    10
-#define MOSI  11
-#define MISO  12
-#define CLK   13
-#define wind A0
-#define DHTPIN 2
+#define CS          4
+#define MOSI        11
+#define MISO        12
+#define CLK         13
+#define wind        A0
+#define BLACKLIGHT  7
+#define DHTPIN      2
 #define DHTTYPE DHT22
 #define OUTPUT_FILE  "Datalog.csv"
 
@@ -42,9 +45,10 @@ DHT dht(DHTPIN, DHTTYPE);
 RTC_DS1307 rtc;
 File wsFile;
 
+unsigned long previous = 0;
 unsigned long previousMillis = 0;
-const long interval = 1000;
 int windspeed = 0;
+int count = 30, state = 0;
 
 byte thermo[8] = {
   B00100,
@@ -96,52 +100,103 @@ void first_save() {
   }
 }
 
+void datalog() {
+  DateTime now = rtc.now();
+  wsFile = SD.open(OUTPUT_FILE, FILE_WRITE);
+  if (wsFile) {
+    wsFile.print(now.day());
+    wsFile.print("/");
+    wsFile.print(now.month());
+    wsFile.print("/");
+    wsFile.print(now.year());
+    wsFile.print(",");
+    wsFile.print(now.hour());
+    wsFile.print(":");
+    wsFile.print(now.minute());
+    wsFile.print(":");
+    wsFile.print(now.second());
+    wsFile.print(",");
+    wsFile.print(dht.readTemperature());
+    wsFile.print(",");
+    wsFile.print(dht.readHumidity());
+    wsFile.print(",");
+    wsFile.println(windspeed);
+    wsFile.close();
+  }
+}
+
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Initial");
+  pinMode(BLACKLIGHT, INPUT_PULLUP);
+
   SD.begin(CS);
   dht.begin();
-  lcd.begin();
 
-  lcd.backlight();
+  rtc.begin();
+  //  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  lcd.begin();
   lcd.createChar(1, thermo);
   lcd.createChar(2, water);
   lcd.createChar(3, Cloud);
+
   lcd.home();
   lcd.print(" Weather Station");
   lcd.setCursor(0, 1);
   lcd.print("     IVEN1      ");
-  delay(1000);
-  lcd.noBacklight();
-  delay(1000);
   lcd.backlight();
-  delay(1000);
-  
+  delay(2000);
+  lcd.noBacklight();
+  delay(500);
+  lcd.backlight();
+  delay(500);
+  lcd.noBacklight();
+
   first_save();
   Serial.println("done...");
 }
 
 void loop() {
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= 1000) {
     int sensorValue = analogRead(A0);
     float outvoltage = sensorValue * (5.0 / 1023.0);
     windspeed = 6 * outvoltage;
     previousMillis = currentMillis;
   }
 
+  if (digitalRead(BLACKLIGHT) == 0) {
+    lcd.backlight();
+    state = 1;
+  }
+
+  if (state == 1) {
+    unsigned long current = millis();
+    if (current - previous >= 30000) {
+      lcd.noBacklight();
+      previous = current;
+      state = 0;
+    }
+  }
+
+  DateTime now = rtc.now();
+
   int t = dht.readTemperature();
   int h = dht.readHumidity();
 
-  //  Serial.print("wind: ");
-  //  Serial.print(windspeed);
-  //  Serial.print(" ");
-  //  Serial.print("Humidity: ");
-  //  Serial.print(h);
-  //  Serial.print(" %\t");
-  //  Serial.print("Temperature: ");
-  //  Serial.print(t);
-  //  Serial.println(" *C ");
+  if (count == 60) count = 59;
+  if (count > 60) count = count - 59;
+
+  if (now.minute() == count) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(" Saving...      ");
+    datalog();
+    delay(2000);
+    count += 30;
+  }
 
   lcd.setCursor(0, 0);
   lcd.print(" ");
@@ -160,4 +215,26 @@ void loop() {
   lcd.print(windspeed);
   lcd.print(" m/s   ");
   delay(100);
+
+  //  Serial.print("wind: ");
+  //  Serial.print(windspeed);
+  //  Serial.print(" ");
+  //  Serial.print("Humidity: ");
+  //  Serial.print(h);
+  //  Serial.print(" %\t");
+  //  Serial.print("Temperature: ");
+  //  Serial.print(t);
+  //  Serial.println(" *C ");
+  //  Serial.print(now.year(), DEC);
+  //  Serial.print('/');
+  //  Serial.print(now.month(), DEC);
+  //  Serial.print('/');
+  //  Serial.print(now.day(), DEC);
+  //  Serial.print(" ");
+  //  Serial.print(now.hour(), DEC);
+  //  Serial.print(':');
+  //  Serial.print(now.minute(), DEC);
+  //  Serial.print(':');
+  //  Serial.print(now.second(), DEC);
+  //  Serial.println();
 }
